6193d874d970c2ee96f6a70d067138d322460dca
Dorian Dubreus <doriandubreus@gmail.com>
2022-04-24 19:29:36 -0400

    laying out the ground work

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
         console.log(data)
-        document.querySelector('img').src = data.drinks[2].strDrinkThumb
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb
     })
     .catch(err => {
         console.log(`error ${err}`)
