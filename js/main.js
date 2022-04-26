2db0479114a7837f73ac12daa024dda4bafc0eec
Dorian Dubreus <doriandubreus@gmail.com>
2022-04-26 18:23:11 -0400

    new branch

--- js/main.js
+++ js/main.js
@@ -1,49 +1,17 @@
 
 
-function getRandomCocktails() {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-            //console.log(data)
-            displayRandomCocktail(data)
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        //console.log(data)
+        console.log(data.drinks[0])
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
 
 
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
 
-}
 
-getRandomCocktails()
 
-function displayRandomCocktail(cocktail) {
-    console.log(cocktail.drinks[0])
-
-    let drinkSection = document.querySelector('#drink-section')
-
-    let drinkName = document.createElement('h2')
-    drinkName.innerHTML = cocktail.drinks[0].strDrink
-
-    drinkSection.appendChild(drinkName)
-
-    let img = document.querySelector('img')
-    img.src = cocktail.drinks[0].strDrinkThumb
-    drinkSection.appendChild(img)
-
-    for (let i = 1; i < 16; i++) {
-        if (cocktail.drinks[0][`strIngredient${i}`] === null) {
-            break;
-        }
-
-        let ingredient = document.createElement('li')
-        ingredient.innerHTML = cocktail.drinks[0][`strMeasure${i}`] + ':' + cocktail.drinks[0][`strIngredient${i}`]
-        drinkSection.appendChild(ingredient)
-    }
-
-    let card = document.createElement('ons-card')
-    card.innerHTML = cocktail.drinks[0].strInstructions
-
-    drinkSection.appendChild(card)
-
-}
+//PULL TO REFRESH 
