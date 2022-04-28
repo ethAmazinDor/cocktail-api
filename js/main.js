6e5e3f358ee1de9dc49ecf4c189351916827e7ea
Dorian Dubreus <doriandubreus@gmail.com>
2022-04-27 20:12:41 -0400

    new index page

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-        //console.log(data)
-        console.log(data.drinks[0])
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
-
-
-
-
 
 //PULL TO REFRESH 
